63d4b08fd6cf700817538eabbc5a6f42b79740fc
booky <booky@booky.dev>
2026-02-11 16:45:59 +0100

    Automatically add own instance to fermi instance list

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env ash
+#!/usr/bin/env sh
 
 set -e
 
 # copy the default instances.json file if it doesn't exist yet
 if [ ! -e "$JANK_INSTANCES_PATH" ]; then
-	cp -v "/app/dist/webpage/instances.json" "$JANK_INSTANCES_PATH"
+  SUBSTITUTED_INSTANCES="$(envsubst < "/setup/jank-extra-instances.json")"
+  echo "$SUBSTITUTED_INSTANCES" | jq -rs '[.[][]]' "-" "/app/dist/webpage/instances.json" > "$JANK_INSTANCES_PATH"
 fi
 
 # launch jank client
